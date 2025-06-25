3b5162bb24e05e3bfe1f98298aaff08ceae744ff
Subham Mondal <subhamoffi@gmail.com>
2025-06-25 21:10:26 +0530

    Added the images from backend

--- FrontEnd/client/src/components/Contents.tsx
+++ FrontEnd/client/src/components/Contents.tsx
@@ -1,12 +1,54 @@
+import { useEffect, useState } from "react";
 import Product from "./Product";
+import axios from "axios";
+
+interface IImage {
+  imageUrl: string;
+  image_id: number;
+}
+interface IProduct {
+  Images: IImage[];
+  price: number;
+  product_id: number;
+  product_name: string;
+}
+
+interface iData {
+  message: string;
+  products: IProduct[];
+}
 
 const Contents = () => {
-  const array = [1, 2, 3, 4, 5, 6];
+  const [productArray, setProductArray] = useState<IProduct[]>([]);
+  const BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URL;
+  const getAllProducts = async () => {
+    const { status, data }: { status: number; data: iData } = await axios.get(
+      `${BACKEND_URL}/product/bulk`
+    );
+    if (status == 200) {
+      setProductArray(data.products);
+    }
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
   return (
     <div className="w-full h-auto grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 px-4 ">
-      {array.map((id) => {
-        return <Product key={id} />;
-      })}
+      {productArray[0] &&
+        productArray.map(({ product_id, Images, price, product_name }) => {
+          // console.log(Images);
+          return (
+            <Product
+              key={product_id}
+              imgUrl={Images[0].imageUrl}
+              price={price}
+              productName={product_name}
+            />
+          );
+        })}
     </div>
   );
 };
--- FrontEnd/client/src/components/Product.tsx
+++ FrontEnd/client/src/components/Product.tsx
@@ -1,21 +1,31 @@
-const Product = () => {
+interface IProps {
+  imgUrl: string;
+  price: number;
+  productName: string;
+}
+
+const Product = ({ imgUrl, price, productName }:IProps) => {
   return (
     <div className="w-full flex gap-3 flex-col h-[400px]">
       <div className="w-full h-3/4">
         <img
-          src="./product2.png"
+          src={`${imgUrl}`}
           className="w-full h-full object-cover rounded-3xl"
-          alt=""
+          alt="Product"
         />
       </div>
       <div className="flex-1 flex justify-between items-center px-2">
         <div className="text-sm font-[Amagro]">
-          <h4 >Barcelona Jersey Player Edition</h4>
-          <h3>&#8377; 699.00 </h3>
+          <h4>{productName}</h4>
+          <h3>&#8377; {Number(price) / 100}.00 </h3>
         </div>
         <div className="bg-(--primary-button) rounded-2xl h-10 px-3 py-2.5 flex items-center">
           <button className="h-full">
-            <img src="./svg/arrow.svg" className="object-cover h-full" alt="alt" />
+            <img
+              src="./svg/arrow.svg"
+              className="object-cover h-full"
+              alt="alt"
+            />
           </button>
         </div>
       </div>
