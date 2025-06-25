37906ad625b6c51318eb88a879bac48c701fde42
Subham Mondal <subhamoffi@gmail.com>
2025-06-25 20:19:58 +0530

    Made a route for getting all the data of products

--- Backend/src/routes/productlist.ts
+++ Backend/src/routes/productlist.ts
@@ -1,4 +1,6 @@
 import { Context, Hono } from "hono";
+import { PrismaClient } from "../generated/prisma/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 export const productRoute = new Hono();
 
@@ -7,3 +9,34 @@ productRoute.get("/", (c: Context) => {
         message: "Hello from product"
     })
 });
+
+productRoute.get("/bulk", async (c: Context) => {
+    try {
+        const prisma = new PrismaClient({
+            datasourceUrl: c.env.DATABASE_URL
+        }).$extends(withAccelerate())
+
+        const allProduct = await prisma.product.findMany(
+            {
+                select: {
+                    Images: true,
+                    price: true,
+                    product_id: true,
+                    product_name: true
+                }
+            }
+        )
+
+        return c.json({
+            message: "All your products",
+            products: allProduct
+        })
+
+
+    } catch (error) {
+        return c.json({
+            message: "Internal Error",
+            error
+        }, 500)
+    }
+})
