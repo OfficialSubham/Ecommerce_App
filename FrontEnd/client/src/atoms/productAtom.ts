375f54e44b20c8886719b76da067c06686db443e
Subham Mondal <subhamoffi@gmail.com>
2025-06-25 21:50:46 +0530

    Everything transfered to atom

---
+++ FrontEnd/client/src/atoms/productAtom.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { atom, selector } from "recoil";
+
+interface IImage {
+    imageUrl: string;
+    image_id: number;
+}
+interface IProduct {
+    Images: IImage[];
+    price: number;
+    product_id: number;
+    product_name: string;
+}
+
+
+interface iData {
+    message: string;
+    products: IProduct[];
+}
+
+export const productState = atom({
+    key: "productState",
+    default: selector({
+        key: "allProduct",
+        get: async ({ get }) => {
+            const BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URL;
+            const { status, data }: { status: number; data: iData } = await axios.get(
+                `${BACKEND_URL}/product/bulk`
+            );
+            if (status == 200) {
+                return data.products
+            }
+            console.log(data);
+        }
+    })
+}) 
--- FrontEnd/client/src/components/Contents.tsx
+++ FrontEnd/client/src/components/Contents.tsx
@@ -1,54 +1,58 @@
-import { useEffect, useState } from "react";
 import Product from "./Product";
-import axios from "axios";
+import { useRecoilValueLoadable } from "recoil";
+import { productState } from "../atoms/productAtom";
+import Loading from "./Loading";
+
+interface IProps {
+  imgUrl: string;
+  price: number;
+  productName: string;
+}
 
 interface IImage {
   imageUrl: string;
-  image_id: number;
-}
-interface IProduct {
-  Images: IImage[];
-  price: number;
-  product_id: number;
-  product_name: string;
-}
-
-interface iData {
-  message: string;
-  products: IProduct[];
 }
 
 const Contents = () => {
-  const [productArray, setProductArray] = useState<IProduct[]>([]);
-  const BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URL;
-  const getAllProducts = async () => {
-    const { status, data }: { status: number; data: iData } = await axios.get(
-      `${BACKEND_URL}/product/bulk`
-    );
-    if (status == 200) {
-      setProductArray(data.products);
-    }
-    console.log(data);
-  };
+  const productsLoadable = useRecoilValueLoadable(productState);
 
-  useEffect(() => {
-    getAllProducts();
-  }, []);
+  if (productsLoadable.state === "loading")
+    return (
+      <div className="w-full h-full flex items-center justify-center">
+        <Loading />
+      </div>
+    );
+  console.log(productsLoadable);
+  // useEffect(() => {
+  //   getAllProducts();
+  // }, []);
 
   return (
     <div className="w-full h-auto grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 px-4 ">
-      {productArray[0] &&
-        productArray.map(({ product_id, Images, price, product_name }) => {
-          // console.log(Images);
-          return (
-            <Product
-              key={product_id}
-              imgUrl={Images[0].imageUrl}
-              price={price}
-              productName={product_name}
-            />
-          );
-        })}
+      {productsLoadable.contents[0] &&
+        productsLoadable.contents.map(
+          ({
+            product_id,
+            Images,
+            price,
+            product_name,
+          }: {
+            product_id: number;
+            price: number;
+            product_name: string;
+            Images: IImage[];
+          }) => {
+            // console.log(Images);
+            return (
+              <Product
+                key={product_id}
+                imgUrl={Images[0].imageUrl}
+                price={price}
+                productName={product_name}
+              />
+            );
+          }
+        )}
     </div>
   );
 };
